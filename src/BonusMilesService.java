edf4d95d88ebf6f05d06961a4d0aeeebbfaeec0b
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-22 19:31:17 +0600

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-    int result = price / 20;
+        int result = price / 20;
 
-    return result;
-}
+        return result;
+    }
 }
